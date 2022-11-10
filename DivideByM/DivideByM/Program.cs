62a8c5a3a17387dd82d7394b86d9c89491a8f5af
Ahmet Aksünger <116587797+AhmetAksunger@users.noreply.github.com>
2022-11-10 21:08:02 +0300

    Add files via upload

---
+++ DivideByM/DivideByM/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivideByM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DivideByM(99, 3);
+            Console.ReadLine();
+        }
+        static int DivideByM(int n, int m)
+        {
+            int i;
+            for (i = 0; i <= n; i++)
+            {
+                if (i % m == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            return i;
+        }
+    }
+}
